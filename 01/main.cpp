6b6520b1b0caded199de6b1d034f9aa377d7e2f7
shirgalin <115062548+shirgalin@users.noreply.github.com>
2023-10-14 12:40:10 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 
 class Calculator {
-public:
+private:
     double num1, num2;
-    double add(double num1, double num2) {
+public:
+    double add() {
         return num1 + num2;
     }
-    double multiply(double num1, double num2) {
+    double multiply() {
         return num1 * num2;
     }
-    double substract_1_2(double num1, double num2) {
+    double substract_1_2() {
         return num1 - num2;
     }
-    double substract_2_1(double num1, double num2) {
+    double substract_2_1() {
         return num2 - num1;
     }
-    double divide_1_2(double num1, double num2) {
+    double divide_1_2() {
         return num1 / num2;
     }
-    double divide_2_1(double num1, double num2) {
+    double divide_2_1() {
         return num2 / num1;
     }
     bool set_num1(double num1) {
@@ -42,13 +43,13 @@ public:
 };
 
 
-void Calc(Calculator c, double& num1, double& num2) {
-    cout << "num1 + num2 = " << c.add(num1, num2) << endl;
-    cout << "num1 - num2 = " << c.substract_1_2(num1, num2) << endl;
-    cout << "num2 - num1 = " << c.substract_2_1(num1, num2) << endl;
-    cout << "num1 * num2 = " << c.multiply(num1, num2) << endl;
-    cout << "num1 / num2 = " << c.divide_1_2(num1, num2) << endl;
-    cout << "num2 / num1 = " << c.divide_2_1(num1, num2) << endl;
+void Calc(Calculator c) {
+    cout << "num1 + num2 = " << c.add() << endl;
+    cout << "num1 - num2 = " << c.substract_1_2() << endl;
+    cout << "num2 - num1 = " << c.substract_2_1() << endl;
+    cout << "num1 * num2 = " << c.multiply() << endl;
+    cout << "num1 / num2 = " << c.divide_1_2() << endl;
+    cout << "num2 / num1 = " << c.divide_2_1() << endl;
 }
 
 
@@ -75,7 +76,7 @@ int main() {
             } while (*num2 <= 0);
         }
         c.set_num2(*num2);
-        Calc(c, *num1, *num2);
+        Calc(c);
     } while(true);
     return 0;
 }
